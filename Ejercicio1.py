91c271ec2497d72574a36682b03c57cf839ccef8
DESKTOP-SRK6ME2\Ricardo <ricardoquinzo@hotmail.com>
2017-05-31 22:52:19 -0500

    Ejercicio 1 y 2 Ricardo Quinzo

---
+++ Ejercicio1.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root = Tk()
+etiqueta1 = Label(root, text="Escuela politecnica nacional")
+etiqueta2 = Label(root, text="Programacion Avanzada")
+etiqueta3 = Label(root, text="2017A")
+etiqueta1.pack()
+etiqueta2.pack()
+etiqueta3.pack()
+root.mainloop()
